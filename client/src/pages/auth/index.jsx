19cb3f2ab580918b7f9e4d14cff0860e726ba42b
Atveek <atveekdungarani2021@gmail.com>
2024-07-12 15:09:14 +0530

    Frontend Part1 : vite + react + shadcn component setup is ready

---
+++ client/src/pages/auth/index.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+
+function Auth() {
+  return (
+    <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+      <div className="h-[80vh] bg-white border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:[70vw] cl:w-[60vw] rounded-3xl  grid xl:grid-cols-2">
+        <div className="flex flex-col gap-10 items-center justify-center">
+          <div className="flex items-center  justify-center flex-col">
+            <div className="flex items-center justify-center">
+              <h1 className="text-5xl font-bold md:text-6xl">Welcome</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Auth;
