270d0b3774b62b771ba4dead918eaf10db8bf575
Atveek <atveekdungarani2021@gmail.com>
2024-07-13 19:45:47 +0530

    Frontend Part 5 : header , message container and message bar   ui is ready in chat container

---
+++ client/src/pages/chat/empty-chat-container/index.jsx
@@ -0,0 +1,25 @@
+import { animationDefaultOption } from "@/lib/utils";
+import Lottie from "react-lottie";
+
+function EmptyChatContainer() {
+  return (
+    <div className="flex-1 md:bg-[#1c1d25] flex-col md:flex justify-center items-center hidden duration-100 transition-all">
+      <Lottie
+        isClickToPauseDisabled={true}
+        height={200}
+        width={200}
+        options={animationDefaultOption}
+      ></Lottie>
+      <div className="text-opacity-80 text-white flex flex-col gap-5 items-center mt-10 lg:text-4xl text-3xl transition-all duration-300 text-center">
+        {" "}
+        <h3 className="poppins-medium">
+          Hi <span className="text-purple-500 ">!</span>Welcome to
+          <span className="text-purple-500"> QuickChat </span> App
+          <span className="text-purple-500">.</span>
+        </h3>
+      </div>
+    </div>
+  );
+}
+
+export default EmptyChatContainer;
--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -4,6 +4,9 @@ import { useAppStore } from "@/store";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import ContactsContainer from "./contacts-container";
+// import EmptyChatContainer from "./empty-chat-container";
+import ChatContainer from "./chat-container";
 
 function Chat() {
   const navigate = useNavigate();
@@ -14,7 +17,13 @@ function Chat() {
       navigate("/profile");
     }
   }, [userInfo, navigate]);
-  return <div>Chat</div>;
+  return (
+    <div className="flex h-[100vh] text-white  overflow-hidden">
+      <ContactsContainer />
+      {/* <EmptyChatContainer /> */}
+      <ChatContainer />
+    </div>
+  );
 }
 
 export default Chat;
